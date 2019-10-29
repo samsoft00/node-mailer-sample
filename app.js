023ba72c65bfb30ecae7a83deba66a3af57b8f66
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2019-10-29 12:34:58 +0100

    update route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+let fs = require('fs');
 
 const MailEvent   = require("./MailEvent");
 const Mailer        = new MailEvent();
@@ -25,11 +26,14 @@ app.use(cors());
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
-app.post('/sendSubscriptionMail', async (req, res, next) => {
+app.post('/sendMail', async (req, res, next) => {
   let {email} = req.body;
   let {password} = req.query;
 
   if(password === process.env.PASSWORD){
+    //check if email exist
+    //if not, register new email and send mail
+    // if true, reply with 'Already sign up, kindly check your mail for confirmation message.
     Mailer.emit('SEND_WELCOME_MAIL', email);
     return res.status(200).send({});
   }
